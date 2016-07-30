39973cdeab55bdd266a7c9807576b6f3028aa4ee
OptimisticCoder <danpatching@gmail.com>
2016-07-30 11:05:30 +0100

    added p2p_manager.

---
+++ include/p2p_manager.hpp
@@ -0,0 +1,21 @@
+#ifndef __P2P_MANAGER_H_INCLUDED__
+#define __P2P_MANAGER_H_INCLUDED__
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace P2PNetwork
+{
+	class p2p_manager
+	{
+	public:
+		p2p_manager();
+		~p2p_manager();
+
+	};
+}
+
+#endif
---
+++ src/p2p_manager.cpp
@@ -0,0 +1,14 @@
+#include "p2p_manager.hpp"
+
+namespace P2PNetwork
+{
+	p2p_manager::p2p_manager()
+	{
+
+	}
+
+	p2p_manager::~p2p_manager()
+	{
+
+	}
+}
